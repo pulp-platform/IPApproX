04be31aa3c39cfe0c164a5fd358e22e97fd8b058
Francesco Conti <f.conti@unibo.it>
2016-02-04 14:52:42 +0100

    added targets for synthesis for various techs

--- SubIPConfig.py
+++ SubIPConfig.py
@@ -15,10 +15,67 @@ class SubIPConfig(object):
         except KeyError:
             self.incdirs = []
 
+        # target-specific source files for the IP (mutually exclusive)
+        self.target_tech = 0
+        self.target_rtl  = 0
+        self.target_fpga = 0
+        try:
+            self.files_umc65 = sub_ip_dic['files_umc65_synth']
+            self.target_tech += 1
+        except KeyError:
+            self.files_umc65 = []
+        try:
+            self.files_st28fdsoi = sub_ip_dic['files_st28fdsoi_synth']
+            self.target_tech += 1
+        except KeyError:
+            self.files_st28fdsoi = []
+        try:
+            self.files_gf28 = sub_ip_dic['files_gf28_synth']
+            self.target_tech += 1
+        except KeyError:
+            self.files_gf28 = []
+        try:
+            self.files_xilinx = sub_ip_dic['files_xilinx_synth']
+            self.target_fpga += 1
+        except KeyError:
+            self.files_xilinx = []
+        try:
+            self.files_umc65 = sub_ip_dic['files_umc65']
+            self.target_tech += 1
+        except KeyError:
+            self.files_umc65 = []
+        try:
+            self.files_st28fdsoi = sub_ip_dic['files_st28fdsoi']
+            self.target_tech += 1
+        except KeyError:
+            self.files_st28fdsoi = []
+        try:
+            self.files_gf28 = sub_ip_dic['files_gf28']
+            self.target_tech += 1
+        except KeyError:
+            self.files_gf28 = []
+        try:
+            self.files_xilinx = sub_ip_dic['files_xilinx']
+            self.target_fpga += 1
+        except KeyError:
+            self.files_xilinx = []
+        try:
+            self.files_rtl = sub_ip_dic['files_rtl']
+            self.target_rtl += 1
+        except KeyError:
+            self.files_rtl = []
+
+        # generic source files of the IP (for all targets)
         try:
             self.files = sub_ip_dic['files']
+            self.target_tech += 1
+            self.target_rtl  += 1
+            self.target_fpga += 1
         except KeyError:
-            print "ERROR: failed to find any files associated to ip %s, sub-ip %s." % (ip_name, sub_ip_name)
+            self.files = []
+
+        if self.target_rtl+self.target_tech+self.target_fpga == 0:
+            print "ERROR: there are no sources associated to ip %s, sub-ip %s. Check its src_files.txt file." % (ip_name, sub_ip_name)
             sys.exit(1)
 
         try:
@@ -31,22 +88,32 @@ class SubIPConfig(object):
         except KeyError:
             self.more_opts = ""
 
-    def export_vsim(self, abs_path, more_opts):
+    def export_vsim(self, abs_path, more_opts, target_tech='st28fdsoi'):
         vlog_cmd = VSIM_PREAMBLE_SUBIP % (self.sub_ip_name)
+        files = self.files[:]
+        files.extend(self.files_rtl)
+        if target_tech=='st28fdsoi':
+            files.extend(self.files_st28fdsoi)
+        elif target_tech=='umc65':
+            files.extend(self.files_umc65)
+        elif target_tech=='gf28':
+            files.extend(self.files_gf28)
         if not self.vhdl:
             vlog_includes = ""
             for i in self.incdirs:
                 vlog_includes += "%s%s/%s" % (VSIM_VLOG_INCDIR_CMD, abs_path, i)
-            for f in self.files:
+            for f in files:
                 vlog_cmd += VSIM_VLOG_CMD % ("%s %s" % (more_opts, self.more_opts), vlog_includes, "%s/%s" % (abs_path, f))
         else:
-            for f in self.files:
+            for f in files:
                 vlog_cmd += VSIM_VCOM_CMD % ("%s %s" % (more_opts, self.more_opts), "%s/%s" % (abs_path, f))
         return vlog_cmd
         
     def export_vivado(self, abs_path, more_opts):
         vivado_cmd = VIVADO_PREAMBLE_SUBIP % (self.sub_ip_name, self.sub_ip_name.upper())
-        for f in self.files:
+        files = self.files[:]
+        files.extend(self.files_xilinx_synth)
+        for f in files:
             vivado_cmd += "    %s/%s \\\n" % (abs_path, f)
         vivado_cmd += VIVADO_POSTAMBLE_SUBIP
         return vivado_cmd
