1abf165bc62475bbf303f1fcd04b78988c69cf0b
Germain Haugou <germain.haugou@iie.ee.ethz.ch>
2017-02-06 09:48:35 +0100

    Added new function for getting IP commit list

--- IPDatabase.py
+++ IPDatabase.py
@@ -359,6 +359,36 @@ class IPDatabase(object):
 
         store_ips_list("new_ips_list.yml", new_ips)
 
+    def get_latest_ips(self, changes_severity='warning', tag_always=False):
+        cwd = os.getcwd()
+        ips = self.ip_list
+        new_ips = []
+        for ip in ips:
+            os.chdir("%s/%s" % (self.ips_dir, ip['path']))
+            commit, err = execute_popen("git log -n 1 --format=format:%H", silent=True).communicate()
+            unstaged_changes, err = execute_popen("git diff --name-only").communicate()
+            staged_changes, err = execute_popen("git diff --name-only").communicate()
+            if staged_changes.split("\n")[0] != "":
+                if changes_severity == 'warning':
+                    print(tcolors.WARNING + "WARNING: skipping ip '%s' as it has changes staged for commit." % ip['name'] + tcolors.ENDC + "\nSolve and commit manually.")
+                    os.chdir(cwd)
+                    continue
+                else:
+                    print(tcolors.ERROR + "ERROR: ip '%s' has changes staged for commit." % ip['name'] + tcolors.ENDC + "\nSolve and commit before trying to get latest version.")
+                    sys.exit(1)
+            if unstaged_changes.split("\n")[0] != "":
+                if changes_severity == 'warning':
+                    print(tcolors.WARNING + "WARNING: skipping ip '%s' as it has unstaged changes." % ip['name'] + tcolors.ENDC + "\nSolve and commit manually.")
+                    os.chdir(cwd)
+                    continue
+                else:
+                    print(tcolors.ERROR + "ERROR: ip '%s' has unstaged changes." % ip['name'] + tcolors.ENDC + "\nSolve and commit before trying to get latest version.")
+                    sys.exit(1)
+            new_ips.append({'name': ip['name'], 'path': ip['path'], 'domain': ip['domain'], 'alternatives': ip['alternatives'], 'group': ip['group'], 'commit': "%s" % commit})
+            os.chdir(cwd)
+
+        store_ips_list("new_ips_list.yml", new_ips)
+
     def export_make(self, abs_path="$(IP_PATH)", script_path="./", more_opts="", target_tech='st28fdsoi'):
         for i in self.ip_dic.keys():
             filename = "%s/%s.mk" % (script_path, i)
